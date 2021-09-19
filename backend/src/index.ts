f444a51d54f7c444740d3429bb8278c953e9322a
Luke Browning <lrbrowning6@gmail.com>
2021-09-19 06:32:40 +0100

    Temp remove google API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,8 +21,8 @@ import { Readable } from "stream";
 
 dotenv.config()
 const db = new FirestoreDb();
-const speechClient = new speech.SpeechClient();
-const languageClient = new language.LanguageServiceClient();
+// const speechClient = new speech.SpeechClient();
+// const languageClient = new language.LanguageServiceClient();
 
 passport.serializeUser(function(user, done) {
     // Saving user
@@ -169,89 +169,92 @@ app.patch('/api/user', checkAuth, async (req, res) => {
  */
 
 app.post('/api/speech', async (req, res) => {
-    const items = ["I love you cactus.", "Hello cactus.", "I hate you cactus."];
+    const items = [
+        {text: "I love you cactus.", score: 1},
+        {text: "Hello cactus.", score: 0},
+        {text: "I hate you cactus.", score: -1}];
     const item = items[Math.floor(Math.random()*items.length)];
 
-    // Detects the sentiment of the text
-    const [result] = await languageClient.analyzeSentiment({document: {
-        content: item,
-        type: "PLAIN_TEXT"
-    }});
-    const sentiment = result.documentSentiment;
-    const score =  sentiment?.score!
-    const magnitude = sentiment?.magnitude!;
-
-    let scoreVal: number;
-
-    // -0.2 <= x <= 0.2
-    if (magnitude <= 0.35) {
-        console.log(`It's neutral`);
-        scoreVal = 0;
-    } else if (sentiment?.score! <= 0) {
-        console.log(`It's negative`);
-        scoreVal = -1;
-    } else {
-        console.log(`It's positive`);
-        scoreVal = 1;
-    }
+    // // Detects the sentiment of the text
+    // const [result] = await languageClient.analyzeSentiment({document: {
+    //     content: item,
+    //     type: "PLAIN_TEXT"
+    // }});
+    // const sentiment = result.documentSentiment;
+    // const score =  sentiment?.score!
+    // const magnitude = sentiment?.magnitude!;
+    //
+    // let scoreVal: number;
+    //
+    // // -0.2 <= x <= 0.2
+    // if (magnitude <= 0.35) {
+    //     console.log(`It's neutral`);
+    //     scoreVal = 0;
+    // } else if (score <= 0) {
+    //     console.log(`It's negative`);
+    //     scoreVal = -1;
+    // } else {
+    //     console.log(`It's positive`);
+    //     scoreVal = 1;
+    // }
 
     const respJson: SpeechResp = {
-        text: item,
-        score: scoreVal
+        text: item.text,
+        score: item.score // scoreVal
     }
 
     res.json(respJson);
 });
 
 
-app.post('/api/speech-prod', (req, res) => {
-    let fstream = concat(convert);
-    req.pipe(req.busboy);
-    req.busboy.on('file', function (fieldname, file, filename) {
-        console.log("Uploading: " + filename);
-
-        file.pipe(fstream);
-        fstream.on('close', function () {
-            console.log("Upload Finished of " + filename);
-            res.sendStatus(200);
-        });
-    });
-
-    function convert(buffer: Buffer) {
-        let goodStream = concat(gotData);
-
-        ffmpeg(Readable.from(buffer))
-            .format('wav')
-            .audioCodec('pcm_s16le')
-            .audioChannels(1)
-            .audioFrequency(16000)
-            .writeToStream(goodStream, { end: true });
-    }
-
-    function gotData(buffer: Buffer) {
-        let base64data = buffer.toString('base64');
-        //console.log(base64data);
-
-        speechClient.recognize({
-            config: {
-                encoding: "LINEAR16",
-                sampleRateHertz: 16000,
-                languageCode: 'en-US',
-            },
-            audio: {
-                content: base64data
-            }
-        }).then(([resp]) => {
-            // @ts-ignore
-            const transcription = resp.results.map(result => result.alternatives[0].transcript).join('\n');
-            console.log('Transcription: ', transcription);
-            res.send(transcription);
-        }).catch((err) => {
-            console.log(err);
-            res.sendStatus(500);
-        });
-    }
-});
+// app.post('/api/speech-prod', (req, res) => {
+//     let fstream = concat(convert);
+//     req.pipe(req.busboy);
+//     req.busboy.on('file', function (fieldname, file, filename) {
+//         console.log("Uploading: " + filename);
+//
+//         file.pipe(fstream);
+//         fstream.on('close', function () {
+//             console.log("Upload Finished of " + filename);
+//             res.sendStatus(200);
+//         });
+//     });
+//
+//     function convert(buffer: Buffer) {
+//         let goodStream = concat(gotData);
+//
+//         ffmpeg(Readable.from(buffer))
+//             .format('wav')
+//             .audioCodec('pcm_s16le')
+//             .audioChannels(1)
+//             .audioFrequency(16000)
+//             .writeToStream(goodStream, { end: true });
+//     }
+//
+//     function gotData(buffer: Buffer) {
+//         let base64data = buffer.toString('base64');
+//         //console.log(base64data);
+//
+//         speechClient.recognize({
+//             config: {
+//                 encoding: "LINEAR16",
+//                 sampleRateHertz: 16000,
+//                 languageCode: 'en-US',
+//             },
+//             audio: {
+//                 content: base64data
+//             }
+//         }).then(([resp]) => {
+//             // @ts-ignore
+//             const transcription = resp.results.map(result => result.alternatives[0].transcript).join('\n');
+//             console.log('Transcription: ', transcription);
+//             res.send(transcription);
+//         }).catch((err) => {
+//             console.log(err);
+//             res.sendStatus(500);
+//         });
+//     }
+// });
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
